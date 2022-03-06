7d156d2c0c4f65022d346be5418dd62d7c24e455
Js <jspaul2003@gmail.com>
2022-03-06 00:42:36 -0800

    Song model setup

--- app/Models/Song.php
+++ app/Models/Song.php
@@ -5,13 +5,13 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
-class Tmppic extends Model
+class Song extends Model
 {
     use HasFactory;
 
     protected $fillable = [
-        'original_filename',
-        'filename',
-        'userid',
+        'link',
+        'title',
+        'duration',
     ];
 }
